0a6a551c54535c51213a76a68dec0472af3ba2ef
yuna14742 <yuna3558@naver.com>
2025-05-15 13:53:23 +0900

    Feat: 5주차 리액트 강의수강, 실습코드

---
+++ 이유나/week5/react_week2_실습/chapter_05/Comment.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+const styles = {
+  wrapper: {
+    margin: 8,
+    padding: 8,
+    display: "flex",
+    flexDirection: "row",
+    border: "1px solid grey",
+    borderRadius: 16,
+  },
+  imageContainer: {},
+  image: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+  },
+  contentContainer: {
+    marginLeft: 8,
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "column",
+  },
+  nameText: {
+    color: "black",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  commentText: {
+    color: "black",
+    fontSize: 16,
+  },
+};
+
+function Comment(props) {
+  return (
+    <div style={styles.wrapper}>
+      <div style={styles.imageContainer}>
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png"
+          style={styles.image}
+        />
+      </div>
+
+      <div style={styles.contentContainer}>
+        <span style={style.nameText}>{props.name}</span>
+        <span style={style.commentText}>{props.Comment}</span>
+      </div>
+    </div>
+  );
+}
+
+export default Comment;
