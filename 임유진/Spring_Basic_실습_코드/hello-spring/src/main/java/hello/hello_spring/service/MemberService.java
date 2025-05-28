a69fb04ff712347ce423c5c67adc872948e2067f
uuzjin <lyj5721@sookmyung.ac.kr>
2025-05-28 22:27:22 +0900

    Feat: 7주차 과제 및 실습 코드

--- 임유진/Spring_Basic_실습_코드/hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
+++ 임유진/Spring_Basic_실습_코드/hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
@@ -2,41 +2,50 @@ package hello.hello_spring.service;
 
 import hello.hello_spring.domain.Member;
 import hello.hello_spring.repository.MemberRepository;
-import hello.hello_spring.repository.MemoryMemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@Transactional
 public class MemberService {
-    
-    private MemberRepository memberRepository;
-
+    private final MemberRepository memberRepository;
 
     public MemberService(MemberRepository memberRepository){
         this.memberRepository = memberRepository;
     }
-    
-    //회원 가입
-    public Long join(Member member){
-        
-        validateDuplicateMember(member); //중복 회원 검증
-        memberRepository.save(member);
-        return member.getId();
+
+    public Long join(Member member) {
+        long start = System.currentTimeMillis();
+        try {
+            validateDuplicateMember(member); //중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join " + timeMs + "ms");
+        }
     }
 
     private void validateDuplicateMember(Member member){
         memberRepository.findByName(member.getName())
-                .ifPresent(m->{
+                .ifPresent(m -> {
                     throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
     }
-    
-    //전체 회원 조회
-    public List<Member> findMembers(){
-        return memberRepository.findAll();
+
+    public List<Member> findMembers() {
+        long start = System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers " + timeMs + "ms");
+        }
     }
 
     public Optional<Member> findOne(Long memberId){
