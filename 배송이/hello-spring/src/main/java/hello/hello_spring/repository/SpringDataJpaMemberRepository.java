1db611f11c94f9ab86072a9eb5c93b3b77f0b3a3
peisonger <realsonge@sookmyung.ac.kr>
2025-05-29 15:45:26 +0900

    Feat: 7주차 과제 제출

--- 배송이/hello-spring/src/main/java/hello/hello_spring/repository/SpringDataJpaMemberRepository.java
+++ 배송이/hello-spring/src/main/java/hello/hello_spring/repository/SpringDataJpaMemberRepository.java
@@ -1,4 +1,12 @@
 package hello.hello_spring.repository;
 
-public class SpringDataJpaMemberRepository {
+import hello.hello_spring.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository{
+
+    @Override
+    Optional<Member> findByName(String name);
 }
