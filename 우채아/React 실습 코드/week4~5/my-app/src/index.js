78a4b926549910df86484c22dec0e4282934276c
chaeeah <dncodk@naver.com>
2025-06-18 16:00:57 +0900

    Feat: 7주차 실습

--- 우채아/React 실습 코드/week4~5/my-app/src/index.js
+++ 우채아/React 실습 코드/week4~5/my-app/src/index.js
@@ -15,6 +15,7 @@ import SignUp from './chapter_11/SignUp';
 import Calculator from './chapter_12/Calculator';
 import ProfileCard from './chapter_13/ProfileCard';
 import DarkOrLight from './chapter_14/DarkOrLight';
+import Blocks from './chapter_15/Blocks';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -97,11 +98,19 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 // );
 
 //Chapter_14
+// root.render(
+//   <React.StrictMode>
+//     <DarkOrLight />
+//   </React.StrictMode>
+// );
+
+//Chapter_15
 root.render(
   <React.StrictMode>
-    <DarkOrLight />
+    <Blocks/>
   </React.StrictMode>
 );
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
